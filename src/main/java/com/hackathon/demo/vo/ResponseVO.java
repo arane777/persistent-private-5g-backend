a2f33f8140ce3e9366b7056273df2acdca98973a
arane777 <126133528+arane777@users.noreply.github.com>
2023-02-25 08:09:18 +0530

    Update ResponseVO.java

--- src/main/java/com/hackathon/demo/vo/ResponseVO.java
+++ src/main/java/com/hackathon/demo/vo/ResponseVO.java
@@ -1,8 +1,16 @@
 package com.hackathon.demo.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Map;
+
 public class ResponseVO {
     private String statusCode;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String statusMessage;
+    
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> responseData;
 
     public String getStatusMessage() {
         return statusMessage;
@@ -19,4 +27,13 @@ public class ResponseVO {
     public void setStatusCode(String statusCode) {
         this.statusCode = statusCode;
     }
+    
+    public Map<String, String> getResponseData() {
+        return responseData;
+    }
+
+    public void setResponseData(Map<String, String> responseData) {
+        this.responseData = responseData;
+    }
 }
+
